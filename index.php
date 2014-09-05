0cb9f9cbdd3ba650255695b67533eb6d64f99be7
Terence Tseng <terence.tseng@gmail.com>
2014-09-05 14:58:09 -0700

    .

--- index.php
+++ index.php
@@ -3,23 +3,15 @@ require_once './vendor/autoload.php';
 use WindowsAzure\Common\ServicesBuilder;
 use WindowsAzure\Table\Models\Entity;
 
-class MasterCardMatch {
-	private static $tableName = 'wepaymcmatch';
-	private static $connString ='CUSTOMCONNSTR_MCMATCH_CONN_STRING';
-	
-	public function __construct() {
-		$this->app = new \Slim\Slim(['table_name' => self::$tableName, 'conn_string' => getenv(self::$connString)]);
-		$this->app->view(new \JsonApiView());
-		$this->app->add(new \JsonApiMiddleware());
-		$this->app->get('name/:name', [$this, 'name']);
-		return $this->app;
-	}
-	
-	public function name($name) {
-		echo "hello";
-		echo $name;
-	}
-}
+$tableName = 'wepaymcmatch';
+$connString ='CUSTOMCONNSTR_MCMATCH_CONN_STRING';
 
-$mc = new MasterCardMatch();
-$mc->run();
+$this->app = new \Slim\Slim(['table_name' => self::$tableName, 'conn_string' => getenv(self::$connString)]);
+$this->app->view(new \JsonApiView());
+$this->app->add(new \JsonApiMiddleware());
+$this->app->get('name/:name', function ($name) {
+	echo $name;
+});
+$this->app->run();
+
+
